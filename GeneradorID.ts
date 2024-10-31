16d8a697d3e6fa66434fe020954bed4d1762ef0a
Diego_Maldonado <diee.maldonado@gmail.com>
2024-10-31 16:52:47 -0300

    Create GeneradorID.ts
    Add GeneradorID.ts

---
+++ GeneradorID.ts
@@ -0,0 +1,20 @@
+export class GeneradorID {
+  private static idGeneradas: number[] = [];
+
+  static generarId(): number {
+    let nuevaId : number;
+    do {
+      nuevaId = Math.floor(Math.random() * 99999);
+    } while (this.idGeneradas.includes(nuevaId));
+    
+    this.idGeneradas.push(nuevaId);
+    return nuevaId;
+  }
+
+  static eliminarId(id: number): void {
+    const index = this.idGeneradas.indexOf(id);
+    if (index !== -1) {
+      this.idGeneradas.splice(index, 1);
+    }
+  }
+}
