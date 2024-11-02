695e524ff75b265acbac06dc25f742df0fa53ef6
Diego_Maldonado <diee.maldonado@gmail.com>
2024-11-02 11:55:29 -0300

    Update menuClientes.ts
    Refactor menu logic

--- menuClientes.ts
+++ menuClientes.ts
@@ -3,6 +3,49 @@ import { Veterinaria } from "./Veterinaria";
 import { Cliente } from './Cliente';
 import { Paciente } from './Paciente';
 
+function verClientes(veterinaria: Veterinaria): void {
+  let clientes: Cliente[] = veterinaria.getClientes();
+  if (clientes.length === 0) {
+    console.log("No existen clientes.");
+  } else {
+    console.table(clientes);
+  }
+}
+
+function verMascotas(veterinaria: Veterinaria): void {
+  let clienteId: number = readlineSync.questionInt("Ingrese el ID del cliente: ");
+  let cliente: Cliente | undefined = veterinaria.getClientes().find((cliente) => cliente.getId() === clienteId);
+  if (cliente) {
+    let mascotas: Paciente[] = veterinaria.getMascotas(clienteId);
+    if (mascotas) {
+      console.log(`Las mascotas del cliente ${cliente.getNombre()} son:`);
+      console.table(mascotas);
+    } else {
+      console.log(`No existen mascotas del cliente ${cliente.getNombre()} ID ${clienteId}.`);
+    }
+  } else {
+    console.log(`No existe el cliente con ID ${clienteId}.`);
+  }
+}
+
+function agregarCliente(veterinaria: Veterinaria): void {
+  let nombre: string = readlineSync.question("Ingrese el nombre del cliente: ");
+  let telefono: number = readlineSync.questionInt("Ingrese el telefono del cliente: ");
+  let nuevoCliente: Cliente = new Cliente(nombre, telefono);
+  veterinaria.ingresarCliente(nuevoCliente);
+  console.log("Cliente agregado correctamente.");
+}
+
+function eliminarCliente(veterinaria: Veterinaria): void {
+  let clienteIdEliminar: number = readlineSync.questionInt("Ingrese el ID del cliente a eliminar: ");
+  if (veterinaria.getClientes().find((cliente) => cliente.getId() ===(clienteIdEliminar))) {
+    veterinaria.eliminarCliente(clienteIdEliminar);
+    console.log("Cliente eliminado correctamente.");
+  } else {
+    console.log(`No existe el cliente con ID ${clienteIdEliminar}.`);
+  }
+}
+
 export function menuClientes(veterinaria: Veterinaria): void {
   console.log("SECCION CLIENTES");
   let enClientes = true;
@@ -12,43 +55,20 @@ export function menuClientes(veterinaria: Veterinaria): void {
       2. Ver mascotas de un cliente
       3. Agregar cliente
       4. Eliminar cliente
-      5. Volver
+      5. Volver al menu principal
       `);
     switch (nuevaAccion) {
       case "1":
-        let clientes: Cliente[] = veterinaria.getClientes();
-        if (clientes.length === 0) {
-          console.log("No existen clientes.");
-        } else {
-          console.table(clientes);
-        }
+        verClientes(veterinaria);
         break;
       case "2":
-        let clienteId: number = readlineSync.questionInt("Ingrese el ID del cliente: ");
-        let cliente: Cliente | undefined = veterinaria.getClientes().find((cliente) => cliente.getId() === clienteId);
-        if (cliente) {
-          let mascotas: Paciente[] = veterinaria.getMascotas(clienteId);
-          if (mascotas) {
-            console.log(`Las mascotas del cliente ${cliente.getNombre()} son:`);
-            console.table(mascotas);
-          } else {
-            console.log(`No existen mascotas del cliente ${cliente.getNombre()} ID ${clienteId}.`);
-          }
-        } else {
-          console.log(`No existe el cliente ${cliente.getNombre()} ID ${clienteId}.`);
-        }
+        verMascotas(veterinaria);
         break;
       case "3":
-        let nombre: string = readlineSync.question("Ingrese el nombre del cliente: ");
-        let telefono: number = readlineSync.questionInt("Ingrese el telefono del cliente: ");
-        let nuevoCliente: Cliente = new Cliente(nombre, telefono);
-        veterinaria.ingresarCliente(nuevoCliente);
-        console.log("Cliente agregado correctamente.");
+        agregarCliente(veterinaria);
         break;
       case "4":
-        let clienteIdEliminar: number = readlineSync.questionInt("Ingrese el ID del cliente a eliminar: ");
-        veterinaria.eliminarCliente(clienteIdEliminar);
-        console.log("Cliente eliminado correctamente.");
+        eliminarCliente(veterinaria);
         break;
       case "5":
         enClientes = false;
